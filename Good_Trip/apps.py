0a3d686b084c4c0be69c881c339d519530a79326
KAZAHhh01 <ergunmakidinoff@gmail.com>
2025-08-06 17:38:28 +0600

    что-то сделал. ВАЖНОЕ

---
+++ Good_Trip/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GoodTripConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Good_Trip'
---
+++ Good_Trip/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def index_view (request):
+    return render (
+        request=request,
+        template_name='Good_Trip/index.html'
+
+    )
